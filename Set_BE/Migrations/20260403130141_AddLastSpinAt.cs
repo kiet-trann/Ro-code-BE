ec8627fb462caf2a77773b52f26b9b71a157ffa5
kiet-trann <kiettuantranle@gmail.com>
2026-04-03 20:03:52 +0700

    update db gacha

---
+++ Set_BE/Migrations/20260403130141_AddLastSpinAt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Set_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastSpinAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
